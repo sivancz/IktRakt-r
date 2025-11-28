92004b077bc5483c7607c0203f45651184c7dada
Rendszergazda <rendszergazda@blathy.info>
2025-11-28 11:23:41 +0100

    Program.cs fixed

--- IktRaktár/IktRaktár/Program.cs
+++ IktRaktár/IktRaktár/Program.cs
@@ -10,6 +10,20 @@ namespace IktRaktár
 
             Storage storage = new Storage();
 
+            storage.Add(new Product(1, "Ceruza", 100));
+            storage.Add(new Product(2, "Toll", 50));
+            storage.Add(new Product(3, "Füzet", 80));
+
+            Console.WriteLine("Raktár rendszer - fejlesztési alap");
+
+            Order order = new Order();
+            order.AddItem(storage.FindById(1), 3);
+            order.AddItem(storage.FindById(2), 2);
+
+            order.SaveToFile("order_1.txt");
+
+
+
         }
     }
 }
