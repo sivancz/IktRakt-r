a4c44ec13c3cc448c5d1dc9d4056633f21a34d06
Rendszergazda <rendszergazda@blathy.info>
2025-11-28 11:07:18 +0100

    masodik_feladat_ID_KeresoEsListazoFunkciok

--- IktRaktár/IktRaktár/Program.cs
+++ IktRaktár/IktRaktár/Program.cs
@@ -2,14 +2,56 @@
 
 namespace IktRaktár
 {
+    /*CsapatTag 2 - Kereső és listázó funkciók []
+- 1 - Keresés ID alapján
+- 2 - Keresés név részlet alapján
+- 3 - Teljes lista formázott megjelenítése
+    A keresések során az ISearchable-t kell használni.
+
+    Táblázatos listázás:
+    ID | Név        | Készlet
+    ----------------------------
+    1  | Ceruza     | 100
+    2  | Toll       | 50
+        */
     internal class Program
     {
         static void Main(string[] args)
+        {   /*ISearchable interface*/
+            public interface ISearchable
         {
-            Console.WriteLine("Hello, World!");
+            int id
+            {
+                get; set;
+            }
+            string name
+            {
+                get; set;
+            }
 
-            Storage storage = new Storage();
 
         }
+        /*Keresés ID alapján*/
+        public static ISearchable FindByID(int id, List<ISearchable> list)
+        {
+           return list.FirstOrDefault(item => item.id == id);
+        }
+        /*Keresés név részlet alapján*/
+        public static List<ISearchable> FindByName(string namePart, List<ISearchable> list)
+        {
+            return list.Where(item => item.name.Contains(namePart)).ToList();
+        }
+
+            
+        /*Teljes lista formázott megjelenítése*/
+        public static void PrintTable(List<ISearchable> list)
+        {
+            Console.WriteLine("ID | Név        | Készlet");
+            Console.WriteLine("----------------------------");
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.id}  | {item.name} | {item.stock}");
+            }
+        }   
     }
 }
